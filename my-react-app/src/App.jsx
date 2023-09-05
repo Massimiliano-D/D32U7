91ce8d8a8b33c42e50f5b7bf105d53569f44dcc9
Massimiliano-D <137884771+Massimiliano-D@users.noreply.github.com>
2023-09-05 16:48:18 +0200

    D32U7

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyNav from "./components/MyNav";
+import Welcome from "./components/Welcome";
+import AllTheBooks from "./components/AllTheBooks";
+import MyFooter from "./components/MyFooter";
+
+function App() {
+  return (
+    <div className="App">
+      <MyNav />
+      <Welcome />
+      <AllTheBooks />
+      <MyFooter />
+    </div>
+  );
+}
+
+export default App;
